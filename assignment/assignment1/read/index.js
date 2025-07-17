8fcfb627308d1ba97c1335c6474cedbae84ec4fe
kamal-poudel <kamalpoudel138@gmail.com>
2025-07-17 10:38:35 +0530

    assignment1 completely done

---
+++ assignment/assignment1/read/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let arr=[]
+fs.readFile("../write/data.txt","utf-8",(err,data)=>{
+    if(err){
+        return console.log(err);
+    }
+   arr =  data.split('\n')
+
+   let newArr = arr.map(elem=>elem.trim()).filter(line=>line.length>0);
+   let finalData = "";
+   console.log(newArr);
+   newArr.forEach((elem)=>{
+    finalData += elem+" ";
+   })
+   fs.writeFile("./final.txt",finalData,(err)=>{
+    if(err){
+        return console.log(err);
+    }
+    console.log("Final Data Added");
+   })
+
+})
---
+++ assignment/assignment2/index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let input = process.argv.slice(2);
+let data = 
+input.forEach((elem)=>{
+
+
+})
